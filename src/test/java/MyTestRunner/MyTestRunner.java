925131678675b679723dbfe675a1bbb942a60e93
swati-patil-16 <111238845+swati-patil-16@users.noreply.github.com>
2025-01-18 23:14:39 +0530

    Update MyTestRunner.java

--- src/test/java/MyTestRunner/MyTestRunner.java
+++ src/test/java/MyTestRunner/MyTestRunner.java
@@ -33,7 +33,7 @@ import org.testng.annotations.DataProvider;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "rerun:target/failedrerun.txt"
         },
-        tags = "@Login"
+        //tags = "@Login"
 
 
 )
